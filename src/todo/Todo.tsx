9fc04ffc58f4c3c8e55deed6d0d0a395c34e7d57
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-23 18:11:44 -0300

    commit 1

---
+++ src/todo/Todo.tsx
@@ -0,0 +1,12 @@
+export const Todo = () => {
+  return (
+    <>
+      <h1>Hola:</h1>
+      <ul>
+        <li>TODO 1</li>
+        <li>TODO 2</li>
+        <li>TODO 3</li>
+      </ul>
+    </>
+  );
+};
